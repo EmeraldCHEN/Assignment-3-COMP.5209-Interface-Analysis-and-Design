2e504c09e465014e3eae77c72e6f22dced98cc4c
Your Name <you@example.com>
2018-09-20 09:34:06 +1200

    update

---
+++ Shoot-em-up Game/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Shoot_em_up_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picTarget = new System.Windows.Forms.PictureBox();
+            this.timer4Target = new System.Windows.Forms.Timer(this.components);
+            this.picAvatar = new System.Windows.Forms.PictureBox();
+            this.picWeapon = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picShootAtT = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picTarget)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeapon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShootAtT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picTarget
+            // 
+            this.picTarget.Image = ((System.Drawing.Image)(resources.GetObject("picTarget.Image")));
+            this.picTarget.Location = new System.Drawing.Point(12, 67);
+            this.picTarget.Name = "picTarget";
+            this.picTarget.Size = new System.Drawing.Size(152, 99);
+            this.picTarget.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTarget.TabIndex = 0;
+            this.picTarget.TabStop = false;
+            this.picTarget.Click += new System.EventHandler(this.picTarget_Click);
+            // 
+            // timer4Target
+            // 
+            this.timer4Target.Interval = 200;
+            this.timer4Target.Tick += new System.EventHandler(this.timer4Target_Tick);
+            // 
+            // picAvatar
+            // 
+            this.picAvatar.BackColor = System.Drawing.Color.Transparent;
+            this.picAvatar.Image = global::Shoot_em_up_Game.Properties.Resources.Peppa_Pigpig;
+            this.picAvatar.Location = new System.Drawing.Point(564, 534);
+            this.picAvatar.Name = "picAvatar";
+            this.picAvatar.Size = new System.Drawing.Size(155, 163);
+            this.picAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAvatar.TabIndex = 1;
+            this.picAvatar.TabStop = false;
+            this.picAvatar.Click += new System.EventHandler(this.picAvatar_Click);
+            // 
+            // picWeapon
+            // 
+            this.picWeapon.Image = ((System.Drawing.Image)(resources.GetObject("picWeapon.Image")));
+            this.picWeapon.Location = new System.Drawing.Point(564, 162);
+            this.picWeapon.Name = "picWeapon";
+            this.picWeapon.Size = new System.Drawing.Size(91, 98);
+            this.picWeapon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWeapon.TabIndex = 2;
+            this.picWeapon.TabStop = false;
+            this.picWeapon.Visible = false;
+            this.picWeapon.Click += new System.EventHandler(this.picWeapon_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(196, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(947, 125);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1. Press ENTER to start\r\n\r\n2. Use LEFT / RIGHT / UP / DOWN arrows on the keyboard" +
+    " to move Avatar side to side / up & down\r\n\r\n3. Press SPACE BAR to shoot the Targ" +
+    "et";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // picShootAtT
+            // 
+            this.picShootAtT.Image = ((System.Drawing.Image)(resources.GetObject("picShootAtT.Image")));
+            this.picShootAtT.Location = new System.Drawing.Point(752, 242);
+            this.picShootAtT.Name = "picShootAtT";
+            this.picShootAtT.Size = new System.Drawing.Size(100, 106);
+            this.picShootAtT.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picShootAtT.TabIndex = 4;
+            this.picShootAtT.TabStop = false;
+            this.picShootAtT.Visible = false;
+            this.picShootAtT.Click += new System.EventHandler(this.picShootAtT_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.Color.White;
+            this.progressBar.ForeColor = System.Drawing.Color.Yellow;
+            this.progressBar.Location = new System.Drawing.Point(16, 11);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(470, 26);
+            this.progressBar.TabIndex = 5;
+            this.progressBar.Value = 100;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.picShootAtT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picWeapon);
+            this.Controls.Add(this.picAvatar);
+            this.Controls.Add(this.picTarget);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.MaximumSize = new System.Drawing.Size(3000, 2222);
+            this.Name = "Form1";
+            this.Text = "Funny Shooting Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picTarget)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeapon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShootAtT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picTarget;
+        private System.Windows.Forms.Timer timer4Target;
+        private System.Windows.Forms.PictureBox picAvatar;
+        private System.Windows.Forms.PictureBox picWeapon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picShootAtT;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Shoot-em-up Game/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// Developer: Yuan Chen (27044044)
+
+// This game is called Funny Shooting Game. 
+
+// Users just need to simply press arrow keys to move the peppa pig(Avatar) to left, right, up or down.
+
+// Press ENTER to start or replay the game while space bar would used to shoot the banana(weapon) towards the target.
+
+namespace Shoot_em_up_Game
+{  public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        private void picTarget_Click(object sender, EventArgs e)
+        {
+        }
+        // Target character would be moving  at the top horizontally at random 
+
+        private void timer4Target_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int x = r.Next(0, Width - picTarget.Width);
+            picTarget.Location = new Point(x, 100);
+
+            // OR below Target character would be moving  at the top from side to side
+
+            //bool stopMove = false;
+
+            //if (picTarget.Left < Width - 200 && stopMove == false)
+            //{
+            //    picTarget.Left += 200;
+            //}
+            // if(picTarget.Location.X > Width - 200)
+            // or
+
+            //if(picTarget.Left > Width)
+            //{
+            //    stopMove = true;
+            //}
+            //if(picTarget.Left > 0 && stopMove == true)
+            //{
+            //    picTarget.Left -= 200;
+            //}
+            //if (picTarget.Location.X < 200)
+            //{
+            //    stopMove = false;
+            //}
+
+        }   // Retrieved from  https://www.codeproject.com/Questions/460897/Random-motion-of-a-picture-box
+
+        private void picAvatar_Click(object sender, EventArgs e)
+        {
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // After pressing ENTER, instruction will invisible 
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                timer4Target.Enabled = true;
+               
+                label1.Visible = false;
+            }
+            // press arrows to move Avatar
+            
+            if (e.KeyCode == Keys.Left)
+            {
+                if (picAvatar.Left > 0)
+                {
+                    picAvatar.Image = Properties.Resources.Peppa_Pig_Left;
+                    picAvatar.Left -= 30;                          
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (picAvatar.Right < Width)
+                {
+                    picAvatar.Left += 30;
+                    picAvatar.Image = Properties.Resources.Peppa_Pigpig;
+
+                    // OR
+                   //  picAvatar.Location = new Point(picAvatar.Location.X + 30, picAvatar.Location.Y);                                       
+                }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (picAvatar.Top > 160 + picTarget.Top)
+                {
+                    picAvatar.Top -= 30;                                 
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (picAvatar.Bottom < Height - 60)
+                {
+                    picAvatar.Top += 30;                             
+                }
+            }
+            // press SPACE BAR to shoot
+            if (e.KeyCode == Keys.Space && progressBar.Value != 0)
+            {
+                picWeapon.Visible = true;
+                picWeapon.Top -= 30;
+
+                if (picWeapon.Top < 0)
+                {
+                picWeapon.Location = new Point(picAvatar.Location.X + 30, picAvatar.Location.Y - 100);
+                }
+            }          
+        } // Retrieved from https://www.mooict.com/c-tutorial-create-a-zombie-survival-shooting-game-in-visual-studio/
+
+        private void picWeapon_Click(object sender, EventArgs e)
+        {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // When the progress bar is NOT 0 once shot
+            if (picTarget.Bounds.IntersectsWith(picWeapon.Bounds) && progressBar.Value > 0)
+            {
+                progressBar.Value -= 25;
+
+                picWeapon.Visible = false;
+
+                picTarget.Visible = false;
+
+                picShootAtT.Visible = true;
+
+                MessageBox.Show("Good shot!");
+                picShootAtT.Visible = false;
+              
+                picTarget.Visible = true;
+            }
+            // When the progress bar is 0 and game over / play again
+            if ( progressBar.Value == 0)
+            {
+                picWeapon.Visible = false;
+
+                picTarget.Visible = false;
+
+                picShootAtT.Visible = true;
+
+                timer4Target.Enabled = false;
+
+                MessageBox.Show("YOU WON!!!GAME OVER\n\n press ENTER to play again");
+
+                playAgain();
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            picWeapon.Visible = false;
+        }
+        private void playAgain()
+        {
+            progressBar.Value = 100;
+
+            timer4Target.Enabled = true;
+
+            picWeapon.Visible = false;
+
+            picTarget.Visible = true;  
+        }
+        private void picShootAtT_Click(object sender, EventArgs e)
+        {
+        }
+        private void progressBar_Click(object sender, EventArgs e)
+        {            
+        }
+    }
+}
